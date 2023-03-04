8055f10d56988b1617103f4f5b1769067a2519fb
jvwebb <49786444+JVWebb@users.noreply.github.com>
2023-03-04 06:48:43 -0800

    Add files via upload

--- BOT.py
+++ BOT.py
@@ -12,10 +12,11 @@ bot = commands.Bot(command_prefix="", case_insensitive=True, description=descrip
 
 def get_card(cmc):
     if cmc < 17 and not cmc < 0:
-        cmccode = ("f:vintage+type:creature+cmc=" + str(cmc))
+        cmccode = ("game:paper+type:creature+cmc=" + str(cmc))
         usingURL = "https://api.scryfall.com/cards/random?q=" + cmccode
         card = requests.get(usingURL)
         cardinfo = json.loads(card.content)
+        print("got mtg card: " + cardinfo["name"] + "\n")
         return cardinfo["name"], cardinfo["image_uris"]["normal"]
     if cmc > 16:
         return ("No creature is powerful enough to warrant more than 16 mana!", "")
@@ -35,6 +36,7 @@ def yugioh_bs(level):
         card = requests.get(usingURL)
         cardinfo = json.loads(card.content)["data"]
         currentcard = cardinfo[random.randint(0,len(cardinfo))]
+        print("got ygo card: " + currentcard["name"] + "\n")
         return currentcard["name"], currentcard["card_images"][0]["image_url"]
     if level > 12:
         return ("No monster is stronger than a level 12!\nExcept that one card, but who cares about that.", "https://images.ygoprodeck.com/images/cards/15862758.jpg")
@@ -50,6 +52,7 @@ async def on_ready():
 @bot.command()
 async def momir(ctx, number: int):
     """Gives a random creature with given mana value."""
+    print("momir " + str(number))
     card = get_card(number)
     embedded = discord.Embed(title=card[0])
     embedded.set_image(url=card[1])
@@ -58,6 +61,7 @@ async def momir(ctx, number: int):
 @bot.command()
 async def yugioh(ctx, number: int):
     """Gives a random monster of the given level."""
+    print("yugioh " + str(number))
     card = yugioh_bs(number)
     embedded = discord.Embed(title=card[0])
     embedded.set_image(url=card[1])
